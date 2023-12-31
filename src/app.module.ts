f09b2352bc6392086818b05a719e1fdedabc6dbd
yusefmuhmed <yusefmuhmedd6766@gmail.com>
2023-12-31 22:45:35 +0200

    Generate modules and controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { CommentModule } from './comment/comment.module';
+import { TopicModule } from './topic/topic.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, CommentModule, TopicModule],
   controllers: [AppController],
   providers: [AppService],
 })
